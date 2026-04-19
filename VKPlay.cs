6464f71b1177612b7b9b2b71eaa3bb7bac119db7
HobbitMage <hobbit.mage@gmail.com>
2026-04-19 12:12:52 +0300

    initial commit of generated by playnite toolbox source

---
+++ VKPlay.cs
@@ -0,0 +1,87 @@
+﻿using Playnite.SDK;
+using Playnite.SDK.Models;
+using Playnite.SDK.Plugins;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace VKPlay
+{
+    public class VKPlay : LibraryPlugin
+    {
+        private static readonly ILogger logger = LogManager.GetLogger();
+
+        private VKPlaySettingsViewModel settings { get; set; }
+
+        public override Guid Id { get; } = Guid.Parse("f019ed68-2a62-403a-b33d-f91e8529a06e");
+
+        // Change to something more appropriate
+        public override string Name => "Custom Library";
+
+        // Implementing Client adds ability to open it via special menu in playnite.
+        public override LibraryClient Client { get; } = new VKPlayClient();
+
+        public VKPlay(IPlayniteAPI api) : base(api)
+        {
+            settings = new VKPlaySettingsViewModel(this);
+            Properties = new LibraryPluginProperties
+            {
+                HasSettings = true
+            };
+        }
+
+        public override IEnumerable<GameMetadata> GetGames(LibraryGetGamesArgs args)
+        {
+            // Return list of user's games.
+            return new List<GameMetadata>()
+            {
+                new GameMetadata()
+                {
+                    Name = "Notepad",
+                    GameId = "notepad",
+                    GameActions = new List<GameAction>
+                    {
+                        new GameAction()
+                        {
+                            Type = GameActionType.File,
+                            Path = "notepad.exe",
+                            IsPlayAction = true
+                        }
+                    },
+                    IsInstalled = true,
+                    Icon = new MetadataFile(@"c:\Windows\notepad.exe")
+                },
+                new GameMetadata()
+                {
+                    Name = "Calculator",
+                    GameId = "calc",
+                    GameActions = new List<GameAction>
+                    {
+                        new GameAction()
+                        {
+                            Type = GameActionType.File,
+                            Path = "calc.exe",
+                            IsPlayAction = true
+                        }
+                    },
+                    IsInstalled = true,
+                    Icon = new MetadataFile(@"https://playnite.link/applogo.png"),
+                    BackgroundImage = new MetadataFile(@"https://playnite.link/applogo.png")
+                }
+            };
+        }
+
+        public override ISettings GetSettings(bool firstRunSettings)
+        {
+            return settings;
+        }
+
+        public override UserControl GetSettingsView(bool firstRunSettings)
+        {
+            return new VKPlaySettingsView();
+        }
+    }
+}
---
+++ VKPlayClient.cs
@@ -0,0 +1,19 @@
+﻿using Playnite.SDK;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VKPlay
+{
+    public class VKPlayClient : LibraryClient
+    {
+        public override bool IsInstalled => true;
+
+        public override void Open()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
